43b5b1841e3e2657740818cfe7e991e3fedded1d
Softbery <softbery@gmail.com>
2024-06-28 23:10:45 +0200

    1.0.6.103

--- Themedit/git-project.sh
+++ Themedit/git-project.sh
@@ -2,11 +2,25 @@
 
 _VERSION=`cat .sbver`
 
+echo ""
+echo ""
 echo "Geting files ..."
+echo ""
 git add .
+sleep 3
+echo ""
+echo ""
 echo "Commiting all files ...: Version $_VERSION"
+echo ""
 git commit -m "$_VERSION"
+sleep 3
+echo ""
+echo ""
 echo "Pushing repository on server GITHUB ..."
+echo ""
 git push
+echo ""
+echo ""
 echo "All done ..."
 echo "..."
+sleep 3
